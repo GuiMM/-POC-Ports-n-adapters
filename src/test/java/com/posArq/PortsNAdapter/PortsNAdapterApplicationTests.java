50dc494bc55f3936d348e8f7863d9e5fbe76fce7
GuiMM <guilherme.macedomoreira@gmail.com>
2024-05-02 07:30:08 -0300

    First commit

---
+++ src/test/java/com/posArq/PortsNAdapter/PortsNAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.posArq.PortsNAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortsNAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
