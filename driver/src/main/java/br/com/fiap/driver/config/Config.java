28d4791c95fb9ae00a942035ed49e3e05a6ea894
45181360807 <isabella.bellinazzi@apoioprodesp.sp.gov.br>
2024-05-18 15:25:52 -0300

    WIP: Implementação Produto

--- driver/src/main/java/br/com/fiap/driver/config/Config.java
+++ driver/src/main/java/br/com/fiap/driver/config/Config.java
@@ -1,14 +1,8 @@
 package br.com.fiap.driver.config;
-
-import br.com.fiap.core.service.HelloService;
-import br.com.fiap.core.ports.HelloServiceInterface;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
-    @Bean
-    public HelloServiceInterface helloInterface(){
-        return new HelloService();
-    }
+
+
 }
